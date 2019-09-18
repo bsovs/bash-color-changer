0124ef9f8c93ea9fc4f3e8d26dfbf5bdf6dd7b6a
Brandon S <brandon.sovran@rogers.com>
2019-09-18 16:39:10 -0400

    Update Color.sh

--- Color.sh
+++ Color.sh
@@ -1,29 +1,47 @@
 #!/bin/bash
 
-if [ "$1" == "--help" ]; then
-        echo ""
-        echo "Usage: source /tmp/`basename $0` [color-code]"
-        echo ""
-        echo "        -l : [list all color codes]"
-        echo "        -b [color-code] : [background color]"
-        echo ""
-        echo ""
-elif [ "$1" == "-l" ]; then
-        for((i=16; i<256; i++)); do
-                    printf "\e[48;5;${i}m%03d" $i;
-                        printf '\e[0m';
-                            [ ! $((($i - 15) % 6)) -eq 0 ] && printf ' ' || printf '\n'
-                    done
-elif [[ $1 =~ ^[0-9]{3} ]]; then
-        printf '\e[38;5;'$1'm'
-        export PS1='\e[38;5;'$1'm''\u@color-'$1'm \W\$  \e[m'
-        if [ "$2" == "-b" ]; then
-                printf '\e[48;5;'$3'm'
-                export PS1='\e[38;5;'$1'm''\e[48;5;'$3'm''\u@color-'$1'm \W\$  \e[m'
-        fi
-elif [ "$1" == "-b" ]; then
-        printf '\e[48;5;'$2'm'
-        export PS1='\e[48;5;'$2'm''\u@color-'$1'm \W\$  \e[m'
+
+if [[ $@ =~ ^.*(-).*$ ]]; then
+        count=1
+        for var in "$@"
+        do
+                ((count++))
+                if [[ $var =~ (-l) ]]; then
+                        for((i=16; i<256; i++)); do
+                                    printf "\e[48;5;${i}m%03d" $i;
+                                            printf '\e[0m';
+                                                    [ ! $((($i - 15) % 6)) -eq 0 ] && printf ' ' || printf '\n'
+                                                    done
+                        elif [[ $var =~ ((-h)|(--help)) ]]; then
+                        echo ""
+                        echo "Usage: source /tmp/`basename $0` [color-code]"
+                        echo ""
+                        echo "        -c [color-code] : [text color]"
+                        echo "        -l : [list all color codes]"
+                        echo "        -b [color-code] : [background color]"
+                        echo "        -s [style] : [style of font; underline, bold, italic, strike, blink]"
+                        echo ""
+                        echo ""
+                elif [[ $var =~ (-c) ]]; then
+                        printf '\e[38;5;'${!count}'m'
+                        export PS1='\e[38;5;'${!count}'m''\u@color-'${!count}'m \W\$  \e[m'
+                elif [[ $var =~ (-b) ]]; then
+                        printf '\e[48;5;'${!count}'m'
+                        export PS1='\e[48;5;'${!count}'m''\u@color-'${!count}'m \W\$  \e[m'
+                elif [[ $var =~ (-s) ]]; then
+                        if [[ ${!count} =~ (bold) ]]; then
+                                echo -e "\e[1m"
+                        elif [[ ${!count} =~ (italic) ]]; then
+                                echo -e "\e[3m"
+                        elif [[ ${!count} =~ (underline) ]]; then
+                                echo -e "\e[4m"
+                        elif [[ ${!count} =~ (strike) ]]; then
+                                echo -e "\e[9m"
+                        elif [[ ${!count} =~ (blink) ]]; then
+                                echo -e "\e[5;7m"
+                        fi
+                fi
+        done
 else
         echo "Not a Valid Command. Use [--help] to Find a List of Valid Commands."
         printf '\e[38;5;255m'
